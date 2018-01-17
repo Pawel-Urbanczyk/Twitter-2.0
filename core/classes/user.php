aad057c94533efab191413b212e4942ebf759da5
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2018-01-17 13:40:39 +0100

    Poprawa błędów ze zmienna password w funkcji login

--- core/classes/user.php
+++ core/classes/user.php
@@ -8,6 +8,33 @@ class User
     {
         $this->pdo = $pdo;
     }
+
+    public function checkInput($var)
+    {
+        $var = htmlspecialchars($var);
+        $var = trim($var);
+        $var = stripcslashes($var);
+        return $var;
+    }
+
+    public function login($email, $password)
+    {
+        $passwordHash = md5($password);
+        $stmt = $this->pdo->prepare("SELECT `user_id` FROM `users` WHERE `email` = :email AND `password` = :password");
+        $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+        $stmt->bindParam(":password", $passwordHash, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_OBJ);
+        $count = $stmt->rowCount();
+
+        if($count > 0){
+            $_SESSION['user_id'] = $user->user_id;
+            header('Location: home.php');
+        }else{
+            return false;
+        }
+    }
 }
 
 ?>
--- includes/login.php
+++ includes/login.php
@@ -1,6 +1,24 @@
 <?php
 
+if(isset($_POST['login']) && !empty($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
+    if(!empty($email) or !empty($password)) {
+        $email = $getFromU->checkInput($email);
+        $password = $getFromU->checkInput($password);
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errorMsg = "Invalid format";
+        }else {
+            if($getFromU->login($email, $password) === false){
+                $errorMsg = "The email or password is incorrect!";
+            }
+        }
+    }else {
+        $errorMsg = "Please enter username and password!";
+    }
+}
 
 ?>
 
@@ -16,6 +34,16 @@
             <li>
                 <input type="checkbox" value="Remember me" />Remember me
             </li>
+            <?php
+                if(isset($error)){
+                    echo '
+                            <li class="error-li">
+                                <div class="span-fp-error">'.$error.'</div>
+                            </li>
+                    ';
+                }
+            ?>
+
         </ul>
     </form>
 </div>
